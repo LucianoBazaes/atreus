acb5358f526c03358ce2f9f8f387c5c87ede9f61
LucianoBazaes <lucho.bazaes@gmail.com>
2022-09-29 22:20:59 -0300

    Segunda Pre-Entrega

--- js/main.js
+++ js/main.js
@@ -1,82 +1,82 @@
-/*
-const componentes = [
-    { nombre: "procesador", precio: 30000 },
-    { nombre: "memoriaRAM", precio: 7000 },
-    { nombre: "monitor", precio: 40000 },
-    { nombre: "gabinete", precio: 12000 },
-]
 
-//declaro mi carro de compras y que esté vacío inicialmente
-let carro = [];
-let nombreUsuario = prompt("Hola, bienvenido a Atreus Store, por favor ingresá tu nombre:");
-let seleccion = prompt("Hola " + nombreUsuario + " ¿desea comprar algún componente de PC? Ingrese si ó no.");
-//declaro un blucle para que la respuesta del usuario sea válida y no ingrese una respuesta que no sea correcta
+// const componentes = [
+//     { nombre: "procesador", precio: 30000 },
+//     { nombre: "memoriaRAM", precio: 7000 },
+//     { nombre: "monitor", precio: 40000 },
+//     { nombre: "gabinete", precio: 12000 },
+// ]
 
-while (seleccion != "si" && seleccion != "no") {
-    alert("Por favor, ingrese una respuesta válida.");
-    seleccion = prompt("Hola, ¿qué componente de PC quiere comprar? Ingrese si ó no.");
-}
+// //declaro mi carro de compras y que esté vacío inicialmente
+// let carro = [];
+// let nombreUsuario = prompt("Hola, bienvenido a Atreus Store, por favor ingresá tu nombre:");
+// let seleccion = prompt("Hola " + nombreUsuario + " ¿desea comprar algún componente de PC? Ingrese si ó no.");
+// //declaro un blucle para que la respuesta del usuario sea válida y no ingrese una respuesta que no sea correcta
 
-if (seleccion === "si") {
-    alert("Elija el/los componente/s de PC que desea comprar:");
-    let componentesUsuario = componentes.map(
-        (componente) => componente.nombre + "" + " $ " + componente.precio + "" + "\n"
-    );
-    alert(componentesUsuario.join(""));
-} else if (seleccion === "no") {
-    alert("Muchas gracias, hasta pronto");
-}
+// while (seleccion != "si" && seleccion != "no") {
+//     alert("Por favor, ingrese una respuesta válida.");
+//     seleccion = prompt("Hola, ¿qué componente de PC quiere comprar? Ingrese si ó no.");
+// }
 
-while (seleccion != "no") {
-    let componente = prompt("Seleccion el/los componente/s que desea agregar al carrito, tipee ESC para dejar de comprar");
-    let precio = 0;
+// if (seleccion === "si") {
+//     alert("Elija el/los componente/s de PC que desea comprar:");
+//     let componentesUsuario = componentes.map(
+//         (componente) => componente.nombre + "" + " $ " + componente.precio + "" + "\n"
+//     );
+//     alert(componentesUsuario.join(""));
+// } else if (seleccion === "no") {
+//     alert("Muchas gracias, hasta pronto");
+// }
 
-        if (componente === "procesador" || componente === "memoriaRAM" || componente === "monitor" || componente === "gabinete") {
-            switch (componente) {
-                case "procesador":
-                    precio = 30000;
-                    break;
-                case "memoriaRAM":
-                    precio = 7000;
-                    break;
-                case "monitor":
-                    precio = 40000;
-                    break;
-                case "gabinete":
-                    precio = 12000;
-                    break;
-                default:
-                    break;
-            }
+// while (seleccion != "no") {
+//     let componente = prompt("Seleccion el/los componente/s que desea agregar al carrito, tipee ESC para dejar de comprar");
+//     let precio = 0;
 
-            let cantidad = prompt("Ingrese la cantidad que desea llevar:");
+//         if (componente === "procesador" || componente === "memoriaRAM" || componente === "monitor" || componente === "gabinete") {
+//             switch (componente) {
+//                 case "procesador":
+//                     precio = 30000;
+//                     break;
+//                 case "memoriaRAM":
+//                     precio = 7000;
+//                     break;
+//                 case "monitor":
+//                     precio = 40000;
+//                     break;
+//                 case "gabinete":
+//                     precio = 12000;
+//                     break;
+//                 default:
+//                     break;
+//             }
 
-            carro.push({ componente, cantidad, precio });
+//             let cantidad = prompt("Ingrese la cantidad que desea llevar:");
 
-        } else if (componente === "ESC") {
-            alert("Muchas gracias por usar nuestro simulador de compras. Hasta pronto!!!");
-            break;
-        } else {
-            alert("El componente ingresado no es válido.");
-        }
-        
+//             carro.push({ componente, cantidad, precio });
 
-        seleccion = prompt("¿Desea continuar comprando? Ingrese si o no");
+//         } else if (componente === "ESC") {
+//             alert("Muchas gracias por usar nuestro simulador de compras. Hasta pronto!!!");
+//             break;
+//         } else {
+//             alert("El componente ingresado no es válido.");
+//         }
 
-        while (seleccion === "no") {
-            alert("Gracias por su compra, hasta pronto");
-            break;
-        }
-}
 
-const total = carro.reduce ((acc, el) => acc + el.precio * el.cantidad, 0)
+//         seleccion = prompt("¿Desea continuar comprando? Ingrese si o no");
+
+//         while (seleccion === "no") {
+//             alert("Gracias por su compra, hasta pronto");
+//             break;
+//         }
+// }
+
+// const total = carro.reduce ((acc, el) => acc + el.precio * el.cantidad, 0)
 
 //DOM
 
-const dom = document.getElementById("dom");
-dom.innerHTML = `Hola el monto total a pagar de nuestro simulador de compras es de: $${total}. Hasta pronto`;
+// const dom = document.getElementById("dom");
+// dom.innerHTML = `Hola el monto total a pagar de nuestro simulador de compras es de: $${total}. Hasta pronto`;
 
-*/
+
 //EVENTOS
 
 class Cliente {
@@ -97,18 +97,26 @@ form.addEventListener("submit", (e) => {
     const nombre = document.getElementById("nombreForm");
     const apellido = document.getElementById("apellidoForm");
     const mail = document.getElementById("mailForm");
-    
+
 
     const cliente = new Cliente(nombre.value, apellido.value, mail.value);
-    clientes.push(cliente);
-    console.log(clientes);
 
+    clientes.push(cliente);
+
+    // Agrego mis objetos que obtuve desde mi formulario al LocalStorage utilizando el formato JSON  con el método stringify().
+    localStorage.setItem("cliente", JSON.stringify(clientes));
+
+
+    console.log(clientes);
+  
     alert(`Hola ${nombre.value}, hemos recibido tu solicitud de formulario, en breve nos pondremos en contacto con vos`);
     form.reset();
-    
+
 });
 
 cancelarForm.onclick = () => {
     alert(`Hola, hemos cancelado tu solicitud de formulario, hasta pronto!`);
 };
+
+
 
