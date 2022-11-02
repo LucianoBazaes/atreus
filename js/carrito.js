e01b15cbe8e49cd139969b1e2cb318575005e8ba
LucianoBazaes <lucho.bazaes@gmail.com>
2022-11-02 17:06:12 -0300

    Proyecto Final

---
+++ js/carrito.js
@@ -0,0 +1,160 @@
+// // Creo la clase constructora de productos
+
+class Producto {
+        constructor(id, nombre, precio, cantidad) {
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.cantidad = cantidad;
+     }
+
+}
+
+// //Creo mis productos para ser almacenados en un array
+
+ const procesador1 = new Producto("procesador1", "Procesador Ryzen 5 4500", 40000, 1);
+ const procesador2 = new Producto("procesador2", "Procesador Ryzen 5 5600", 50000, 1);
+ const procesador3 = new Producto("procesador3", "Procesador Ryzen 7 5800x", 60000, 1);
+ const procesador4 = new Producto("procesador4", "Procesador Ryzen 9 5900x", 90000, 1);
+
+ const procesador5 = new Producto("procesador5", "Procesador Intel Core I3", 30000, 1);
+ const procesador6 = new Producto("procesador6", "Procesador Intel Core I5", 45000, 1);
+ const procesador7 = new Producto("procesador7", "Procesador Intel Core I7", 70000, 1);
+ const procesador8 = new Producto("procesador8", "Procesador Intel Core I9", 100000, 1);
+
+ const motherBoard1 = new Producto("mother1", "Mother B550 Aorus", 63000, 1);
+ const motherBoard2 = new Producto("mother2", "Mother Gigabyte B550M", 28000, 1);
+ const motherBoard3 = new Producto("mother3", "Mother 1200 Asus Prime", 47000, 1);
+ const motherBoard4 = new Producto("mother4", "Mother B660M Aorus", 55000, 1);
+
+ const memoriaRam1 = new Producto("ram1", "Memoria RAM DDR4 3000MHZ 16GB Corsair", 28000, 1);
+ const memoriaRam2 = new Producto("ram2", "Memoria RAM DDR4 3200MHZ 16GB Corsair", 41000, 1);
+ const memoriaRam3 = new Producto("ram3", "Memoria RAM DDR4 3600MHZ 16GB Kingstone", 30000, 1);
+ const memoriaRam4 = new Producto("ram4", "Memoria RAM DDR4 4000MHZ 16GB Pny", 32000, 1);
+
+ const placaVideo1 = new Producto("video1", "Placa De Video Amd Radeon Asus Dual Rx 6750 Xt", 172000, 1);
+ const placaVideo2 = new Producto("video2", "Placa De Video Asus Tuf Amd Radeon Rx 6900", 270000, 1);
+ const placaVideo3 = new Producto("video3", "Placa De Video Geforce Rtx 2060 Ko 6gb Evga", 102000, 1);
+ const placaVideo4 = new Producto("video4", "Placa De Video Geforce Rtx 3070 Evga Ftw3 Ultra", 194000, 1);
+
+
+
+ //Agrego todos mis productos a un array
+
+ const productos = [procesador1, procesador2, procesador3, procesador4,
+     procesador5, procesador6, procesador7, procesador8,
+     motherBoard1, motherBoard2, motherBoard3, motherBoard4,
+     memoriaRam1, memoriaRam2, memoriaRam3, memoriaRam4,
+    placaVideo1, placaVideo2, placaVideo3, placaVideo4];
+
+
+// //Inicializo mi carrito vacío
+
+ const carroCompras = [];
+
+//Creando la lógica para que mis productos seleccionados se visualicen dentro del carrito
+const contenedorCarrito = document.querySelector('.contenedorCarrito');
+
+
+// Me traigo todos los botones de comprar y les asocio un evento
+const addProducto = document.querySelectorAll('.addCarrito')
+ 
+// Por cada boton "COMPRAR", voy asociarle un evento
+addProducto.forEach(addProductoBoton => {
+   addProductoBoton.addEventListener('click', agregarAlCarrito);
+})
+
+
+//funcion para agregar productos al carrito mediante el boton "COMPRAR"
+function agregarAlCarrito(event){
+  //guardo en una variable hacia donde apunta mi evento
+  const boton = event.target;
+  //me traigo el evento mas cercano a las clase padre, en este caso la clase card es la que me engloba todo lo que necesito
+  const card = boton.closest('.card');
+
+  const subtituloCard = card.querySelector('.subtituloCard').textContent; //me muestra el solo el contenido del nodo (texto)
+  const precio = card.querySelector('.precios').textContent; //me muestra el solo el contenido del nodo (texto)
+  const imagen = card.querySelector('.imgProducto').src ; //me muestra el solo el contenido del nodo (img)
+ 
+  //Llamo a la funcion que se encarga de crear el html dentro del carrito
+  agregarTituloImagenPrecio(subtituloCard,precio,imagen);
+}
+
+
+//funcion que se encarga de crear el html dentro del carrito
+ function agregarTituloImagenPrecio(subtituloCard,precio,imagen) {
+  const divProducto = document.createElement('div');
+  const contenidoCarrito = `
+     <div class="row carroItem">
+                  <div class="col-6">
+                      <div d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                        <img src=${imagen} class="imagen" width="50" height="50">
+                        <h6 class="ml-3 mb-0">${subtituloCard}</h6>
+                      </div>
+                  </div>
+
+            <div class="col-2">
+               <div class="d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                 <p class="precios item mb-0">${precio}</p>
+               </div>
+            </div>
+
+             <div class="col-4">
+                   <div
+                      class="d-flex justify-content-between align-items-center h-100  pb-2 pt-3">
+                      <input class="productoCantidad" type="hidden"
+                          value="1">
+                      <button class="btn btn-danger buttonDelete" type="button">X</button>
+                   </div>
+             </div>      
+    </div>`;
+
+   divProducto.innerHTML = contenidoCarrito;
+
+   contenedorCarrito.appendChild(divProducto);
+
+
+//Eliminar producto del carrito
+   const eventoBotonEliminar = divProducto.querySelector('.buttonDelete');
+   calcularTotalCompra();
+   eventoBotonEliminar.addEventListener('click', eliminarProductoCarrito);
+
+   //llamo a la función que me da el total de la compra
+   calcularTotalCompra();
+}
+
+function calcularTotalCompra() {
+    let total = 0;
+    const totalCompra = document.querySelector('.totalCompra');
+    const carroItems = document.querySelectorAll('.carroItem');
+    
+    carroItems.forEach(carroItem => {
+      const precioProducto = carroItem.querySelector('.precios');
+      const precioElemento = Number(precioProducto.textContent.replace('$', ""));
+      const cantidadProducto = carroItem.querySelector('.productoCantidad')
+      const valorCantidadProducto = Number(cantidadProducto.value);
+      total = total + (precioElemento * valorCantidadProducto); 
+    })
+    totalCompra.innerHTML = `$${total}`;
+}
+
+//funcion que me borra el elemento dentro del carrito
+
+function eliminarProductoCarrito(event) {
+    const botonEliminarProducto = event.target;
+    botonEliminarProducto.closest('.carroItem').remove();
+    calcularTotalCompra();
+    
+}
+
+//funcion que me cambia la cantidad del producto
+
+function cambiarCantidadProducto(event) {
+    const botonCantidad = event.target;
+   if(botonCantidad.value <= 0){
+    botonCantidad.value = 1;
+   }
+   calcularTotalCompra();
+}
+
+//Evento para cuando el cliente presiona el boton comprar 
--- js/main.js
+++ js/main.js
@@ -1,84 +1,5 @@
 
-//  const componentes = [
-//      { nombre: "procesador", precio: 30000 },
-//      { nombre: "memoriaRAM", precio: 7000 },
-//      { nombre: "monitor", precio: 40000 },
-//      { nombre: "gabinete", precio: 12000 },
-//  ]
-
-//  //declaro mi carro de compras y que esté vacío inicialmente
-//  let carro = [];
-//  let nombreUsuario = prompt("Hola, bienvenido a Atreus Store, por favor ingresá tu nombre:");
-//  let seleccion = prompt("Hola " + nombreUsuario + " ¿desea comprar algún componente de PC? Ingrese si ó no.");
-//  //declaro un blucle para que la respuesta del usuario sea válida y no ingrese una respuesta que no sea correcta
-
-//  while (seleccion != "si" && seleccion != "no") {
-//      alert("Por favor, ingrese una respuesta válida.");
-//      seleccion = prompt("Hola, ¿qué componente de PC quiere comprar? Ingrese si ó no.");
-//  }
-
-//  if (seleccion === "si") {
-//      alert("Elija el/los componente/s de PC que desea comprar:");
-//      let componentesUsuario = componentes.map(
-//          (componente) => componente.nombre + "" + " $ " + componente.precio + "" + "\n"
-//      );
-//      alert(componentesUsuario.join(""));
-//  } else if (seleccion === "no") {
-//      alert("Muchas gracias, hasta pronto");
-//  }
-
-//  while (seleccion != "no") {
-//      let componente = prompt("Seleccion el/los componente/s que desea agregar al carrito, tipee ESC para dejar de comprar");
-//      let precio = 0;
-
-//          if (componente === "procesador" || componente === "memoriaRAM" || componente === "monitor" || componente === "gabinete") {
-//              switch (componente) {
-//                  case "procesador":
-//                      precio = 30000;
-//                    break;
-//                  case "memoriaRAM":
-//                      precio = 7000;
-//                      break;
-//                  case "monitor":
-//                     precio = 40000;
-//                     break;
-//                 case "gabinete":
-//                     precio = 12000;
-//                     break;
-//                 default:
-//                      break;
-//              }
-
-//              let cantidad = prompt("Ingrese la cantidad que desea llevar:");
-
-//             carro.push({ componente, cantidad, precio });
-
-//          } else if (componente === "ESC") {
-//              alert("Muchas gracias por usar nuestro simulador de compras. Hasta pronto!!!");
-//             break;
-//         } else {
-//              alert("El componente ingresado no es válido.");
-//         }
-
-
-//         seleccion = prompt("¿Desea continuar comprando? Ingrese si o no");
-
-//         while (seleccion === "no") {
-//            alert("Gracias por su compra, hasta pronto");
-//              break;
-//         }
-//  }
-
-//  const total = carro.reduce ((acc, el) => acc + el.precio * el.cantidad, 0)
-
-//DOM
-
-// const dom = document.getElementById("dom");
-// dom.innerHTML = `Hola el monto total a pagar de nuestro simulador de compras es de: $${total}. Hasta pronto`;
-
-
-//EVENTOS
-
+// ----------- JS PARA FORMULARIO -----------
 class Cliente {
     constructor(nombre, apellido, mail) {
         this.nombre = nombre;
@@ -100,17 +21,7 @@ form.addEventListener("submit", (e) => {
     const mail = document.getElementById("mailForm");
     const asunto1 = document.getElementById("asunto1");
 
-    // Aplico operador ternario 
-    // En lugar de hacer:
-    //    if (nombre.value === "") {
-    //     alert("Por favor ingresá tu nombre.");    
-    // } else {
-    //     alert(`Hola ${nombre.value}, hemos recibido tu solicitud de formulario, en breve nos pondremos en contacto con vos`) 
-    // } 
-    // simplificamos el código de la siguiente manera;
-
-    // (nombre.value === "") ? alert("Por favor ingresá tu nombre.") : alert(`Hola ${nombre.value}, hemos recibido tu solicitud de formulario, en breve nos pondremos en contacto con vos`) ;
-    if ((nombre.value === "") || (apellido.value === "") || (mail.value === "")) {
+    if ((nombre.value === "") || (apellido.value === "") || (mail.value === "") || (asunto1.value === "")){
         Swal.fire({
             title: "Los campos del formulario no pueden estar vacíos",
             text: "Formulario incompleto",
@@ -173,7 +84,5 @@ form.addEventListener("submit", (e) => {
     console.log(clientes);
 
 });
-
-
 
 
